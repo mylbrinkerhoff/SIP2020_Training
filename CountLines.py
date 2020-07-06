f3a51025d6af312bdae039bca61b79ba75b73c54
Mykel Brinkerhoff <mykelbrinkerhoff@Mykels-iMac.local>
2020-07-06 10:35:14 -0700

    Added files for working with STDIN

---
+++ CountLines.py
@@ -0,0 +1,24 @@
+########################################################################
+# 
+# CountLines.py
+#
+# This program will count the number of lines in a text tht is supplied 
+# by the user in STDIN
+#
+# M. Brinkerhoff  *  SIP2020, UCSC  *  2020 July 6 (M)
+#
+########################################################################
+
+# Modules to read in files from stdin
+import fileinput as fi
+
+# Variables
+count = 0
+
+# Loop over the STDIN and count the number of lines in the file
+for line in fi.input():
+    count += 1
+    print(f'{count} {line}')
+
+# Print out the line number count
+print(f'The number of lines in the text is {count}')
