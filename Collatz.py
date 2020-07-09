ba077f14cbc5c500329377106ff7d268d66e24b3
Mykel Brinkerhoff <mykelbrinkerhoff@Mykels-iMac.local>
2020-07-09 09:15:32 -0700

    Added another way to test if odd

--- Collatz.py
+++ Collatz.py
@@ -17,10 +17,18 @@ while n > 1:
     print(int(n))
 
     # if n is even divide 2 otherwise multiply by 3 and add 1
+
+    #Testing if it is even first
     if n % 2 == 0:
         n /= 2
     else:
         n = n * 3 + 1
+    
+    # Testing if it is odd first
+    # if n % 2 == 1:
+    #     n = n * 3 + 1
+    # else: 
+    #     n /= 2  
 
     # if n is equal to 1
     if n == 1:
