c5dacd20afc14f6ac9517742836247fa7552abbf
Myke <mlbrinke@ucsc.edu>
2020-07-13 12:01:09 -0700

    Fixed print statment to remove trailing \n

--- SelectVowels.py
+++ SelectVowels.py
@@ -20,12 +20,15 @@ vowel_pattern = re.compile("^(a|e|i|o|u)")
 
 # Looping through the file one line at a time.
 for line in sonnet:
+    line = line.rstrip("\n")
     string = line.split("\\") # Spliting the string at the \
     orth = string[1] # Store the orthography in orth
     pron = string[6] # Store the pronunciation in pron
 
+    # If loop that will test if the orthography meets the patten and then print out the
+    # orthography and pronunciation tab-seperated
     if vowel_pattern.search(orth):    
-        # print(f'{orth}\t{pron}')
+        print(line)
         # print(orth, pron, sep = '\t')
 
 sonnet.close()
